6f01718955acaca0e1677b88e0aad862671701c6
onting <hemool123@naver.com>
2021-06-20 19:33:06 +0900

    Simple change

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, jsonify, url_for
+from flask import Flask, url_for
 from infer import model_answer
 
 app = Flask(__name__)
 
 @app.route("/")
 def routes_info():
-    links = []
+    links = {}
     for rule in app.url_map.iter_rules():
         defaults = rule.defaults if rule.defaults is not None else ()
         arguments = rule.arguments if rule.arguments is not None else ()
         if "GET" in rule.methods and len(defaults) >= len(arguments):
             url = url_for(rule.endpoint, **(rule.defaults or {}))
-            links.append((url, rule.endpoint))
-    return jsonify(links)
+            links[url] = rule.endpoint
+    return links
 
 @app.route("/qa/<string:question>")
 def qa(question):
