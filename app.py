ea4a43581a2e51c10c84573a79274c92a9524ef8
Kwangyeon Kim <ghemool123@gmail.com>
2022-04-05 16:05:41 +0900

    Add base code on app

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
-from infer import model_answer
+from flask import (
+    Flask, request, jsonify, redirect
+)
+import models
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=('GET', 'POST'))
+def index():
+    if request.method == 'POST':
+        data = request.get_json()
+        question = data.msg
+        answer = dict()
+        # infer should be here
+        answer['answerType'] = "normal"
+        answer['elapsedTime'] = 0
+        answer['msg'] = "임시 답변"
+        return jsonify(answer)
+    
+    return redirect('http://127.0.0.1/')
 
-@app.route("/qa/<string:question>")
-def qa(question):
-    return {
-        "answer": model_answer(question),
-    }
