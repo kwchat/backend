7d71bab2c0b52a766cfce489d926bff7116a1308
Kwangyeon Kim <ghemool123@gmail.com>
2022-04-07 06:09:02 +0000

    chores

--- train.py
+++ train.py
@@ -1,3 +1,9 @@
 import tensorflow as tf
 from official.nlp import optimization
 from models import *
+
+# fine-tune bert for question answering
+# train doc reader
+# train doc retriever
+# train classifier
+# train composed chatter
